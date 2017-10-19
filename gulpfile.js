93c4725603a1c0dddae4f67b23f9a7acf5971880
Vincent Chavez <vincent.chavez@rhythmagency.com>
2017-10-19 13:16:26 -0700

    add menu and slider

--- gulpfile.js
+++ gulpfile.js
@@ -11,16 +11,22 @@ var bourbon = require('node-bourbon').includePaths;
 var neat = require('node-neat').includePaths;
 // process plugins
 var browserSync = require('browser-sync').create();
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var useref = require('gulp-useref');
 var gulpIf = require('gulp-if');
-var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
+var print = require('gulp-print');
+
+const babel = require('gulp-babel');
 
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: 'src'
+            baseDir: 'dist'
         }
     })
 });
@@ -30,23 +36,50 @@ gulp.task('sass', function() {
         .pipe(sass({
             includePaths: ['styles'].concat(bourbon).concat(neat)
         }))
-        .pipe(gulp.dest('src/css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
+gulp.task('js', function() {
+    var b = browserify({
+        entries: './src/js/app.js',
+        debug: true
+      })
+    .transform(babelify.configure({
+        presets: ["env"]
+      }));
+
+    return b.bundle()
+        .pipe(source('app.js'))
+        .pipe(buffer())
+        .pipe(babel({ presets: ['env'] }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+})
+
 gulp.task('useref', function() {
     return gulp.src('src/*.html')
         .pipe(useref())
-        .pipe(gulpIf('*.js', uglify()))
         .pipe(gulpIf('*.css', cssnano()))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('images', function() {
     return gulp.src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
         .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src('src/css/fonts/**/*.+(eot|otf|svg|ttf|woff)')
+        .pipe(gulp.dest('dist/css/fonts'));
 });
 
 gulp.task('clean:dist', function() {
@@ -55,19 +88,19 @@ gulp.task('clean:dist', function() {
 
 gulp.task('watch', ['browser-sync', 'sass'], function() {
     gulp.watch('src/scss/**/*.scss', ['sass']);
-    gulp.watch('src/*.html', browserSync.reload);
-    gulp.watch('src/js/**/*.js', browserSync.reload);
+    gulp.watch('src/*.html', ['useref']);
+    gulp.watch('src/js/**/*.js', ['js']);
 });
 
 gulp.task('build', function (callback) {
   runSequence('clean:dist',
-    ['sass', 'useref', 'images'],
+    ['js', 'sass', 'useref', 'images', 'fonts'],
     callback
   )
 });
 
 gulp.task('default', function (callback) {
-    runSequence(['sass','browser-sync', 'watch'],
+    runSequence(['js', 'sass', 'useref', 'images', 'fonts', 'browser-sync', 'watch'],
         callback
     )
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,32 @@
-console.log('testing')
+import { Slider, SliderItem } from 'vue-easy-slider'
+
+new Vue({
+    el: '#slideshow',
+    data() {
+        return {
+            items: [
+                { name: 'django', backgroundImage: 'django.jpg', headline: 'The IPHONE UNCHAINED.', text: 'Tarantino’s outrageous film Djano Unchained needed an equally in your face mobile experience. Bloody good awesomeness ensued.' },
+                { name: 'vizio', backgroundImage: 'vizio.jpg', headline: 'SAY HELLO TO THE future of VIZIO.', text: 'When Vizio needed to pull the wrap off its new family of PCs they called on Envoy to conceptualize, direct and produce a series of short product films.' },
+                { name: 'escape', backgroundImage: 'escape.jpg', headline: 'HELPING Earth’s Greatest Secrets break out.', text: 'Activate rocket boots! We created a series of out of this world mobile apps and games to introduce the hilarious animated film Escape From Planet Earth.' },
+                { name: 'utlra', backgroundImage: 'ultra.jpg', headline: 'WORLD MEET ULTRA. ULTRA MEET WORLD.', text: 'Asked to launch T-Mobile’s new internationally-minded brand, Ultra Mobile, we created a campaign that’s creativity knows no borders.' }
+            ]
+        }
+    },
+    computed: {
+        getHeight() {
+            return '675px';
+        }
+    },
+    methods: {
+        getStyle(item) {
+            var style = `background-image: url(/images/${ item.backgroundImage }); background-repeat: no-repeat;`;
+            style += `height: 675px;`;
+            
+            return style;
+        }
+    },
+    components: {
+        'Slider': Slider,
+        'SliderItem': SliderItem
+    }
+})
